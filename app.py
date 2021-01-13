f6e02d72bcb461faae6d0f63c7ab8a41b2ff24b2
Stephen Baldwin <44933264+sm4baldwin@users.noreply.github.com>
2021-01-13 13:12:30 -0500

    config setup + environment variables

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask
-app = Flask(__name__)
 
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
@@ -12,3 +14,4 @@ def hello_name(name):
 
 if __name__ == '__main__':
     app.run()
+    print(os.environ['APP_SETTINGS'])
